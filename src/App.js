829ea9528e5b207312540358dd324c6591723a57
LorisHuetter <tidepodislovetidepodislife@gmail.com>
2021-11-19 13:39:03 +0100

    Removal of not used Components
    4 Not used Components have been removed and app.js have been changed, to
      not import those removed Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import logo from './bbw.svg';
-import Sinus from './Components/Sinus';
-import Cosinus from './Components/Cosinus';
-import Tangenz from './Components/Tangenz';
-import Pythagoras from './Components/Pythagoras';
 import Dreieckidentifizierung from './Components/Dreieckidentifizierung';
 import './App.css';
 
@@ -12,10 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="bbw-logo" />
         <Dreieckidentifizierung/>
-        <Pythagoras/>
-        <Sinus/>
-        <Cosinus/>
-        <Tangenz/>
       </header>
     </div>
   );
